e7349dfc1a5c4e0746af3c02babf7510063dd153
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2015-01-10 17:16:14 +1030

    Updated exercise 1.41

--- chapter1/exercise1-41.scm
+++ chapter1/exercise1-41.scm
@@ -29,30 +29,112 @@
 
 ; Now what is the value of the expression (((double (double double)) inc) 5) ?
 ; We can use the substitution model to figure this out.
+; (Alternatively, you could save yourself the time and just run it, since the
+; working is kind of long and tedious!)
 
-; To keep things from getting too unwieldy, we'll figure out what that 
-; (double double) expression evaluates to first, using the fact that we can
-; represent double as (lambda (f) (lambda (x) (f (f x)))) :
+; Note that we can write double as (lambda (f) (lambda (x) (f (f x)))).
+; We'll expand this expression all the way out and see what happens:
 
-(double double)
+;; (((double (double double)) inc) 5)
+
+;; (((double ((lambda (f) (lambda (x) (f (f x))))
+;;            (lambda (f) (lambda (x) (f (f x)))))) inc) 5)
+
+;; (((double (lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                        ((lambda (f) (lambda (x) (f (f x)))) x)))) inc) 5)
+
+;; ((((lambda (f) (lambda (x) (f (f x))))
+;;    (lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                 ((lambda (f) (lambda (x) (f (f x)))) x)))) inc) 5)
+
+;; (((lambda (x) 
+;;     ((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                   ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;      ((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                    ((lambda (f) (lambda (x) (f (f x)))) x))) x))) inc) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   ((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                 ((lambda (f) (lambda (x) (f (f x)))) x))) inc)) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   ((lambda (f) (lambda (x) (f (f x))))
+;;    ((lambda (f) (lambda (x) (f (f x)))) inc))) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   ((lambda (f) (lambda (x) (f (f x))))
+;;    (lambda (x) (inc (inc x))))) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   (lambda (x) 
+;;     ((lambda (x) (inc (inc x)))
+;;      ((lambda (x) (inc (inc x))) x)))) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   (lambda (x) 
+;;     ((lambda (x) (inc (inc x)))
+;;      (inc (inc x))))) 5)
+
+;; (((lambda (x) ((lambda (f) (lambda (x) (f (f x))))
+;;                ((lambda (f) (lambda (x) (f (f x)))) x)))
+;;   (lambda (x) (inc (inc (inc (inc x)))))) 5)
+
+;; (((lambda (f) (lambda (x) (f (f x))))
+;;    ((lambda (f) 
+;;       (lambda (x) (f (f x)))) 
+;;     (lambda (x) (inc (inc (inc (inc x))))))) 5)
+
+;; (((lambda (f) (lambda (x) (f (f x))))
+;;   (lambda (x) ((lambda (x) (inc (inc (inc (inc x)))))
+;;                ((lambda (x) (inc (inc (inc (inc x))))) x)))) 5)
+
+;; (((lambda (f) (lambda (x) (f (f x))))
+;;   (lambda (x) ((lambda (x) (inc (inc (inc (inc x)))))
+;;                (inc (inc (inc (inc x))))))) 5)
+
+;; (((lambda (f) (lambda (x) (f (f x))))
+;;   (lambda (x) (inc (inc (inc (inc (inc (inc (inc (inc x)))))))))) 5)
+
+;; ((lambda (x)
+;;    ((lambda (x) (inc (inc (inc (inc (inc (inc (inc (inc x)))))))))
+;;     ((lambda (x) (inc (inc (inc (inc (inc (inc (inc (inc x))))))))) x))) 5)
+
+;; ((lambda (x)
+;;    ((lambda (x) (inc (inc (inc (inc (inc (inc (inc (inc x)))))))))
+;;     (inc (inc (inc (inc (inc (inc (inc (inc x)))))))))) 5)
+
+;; ((lambda (x)
+;;    (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc x))))))))))))))))) 5)
 
 
+; We've FINALLY reached the end. Now we can wrap this up to get the answer...
+
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 5))))))))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 6)))))))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 7))))))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 8)))))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 9))))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 10)))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 11))))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc (inc 12)))))))))
+;; (inc (inc (inc (inc (inc (inc (inc (inc 13))))))))    
+;; (inc (inc (inc (inc (inc (inc (inc 14)))))))
+;; (inc (inc (inc (inc (inc (inc 15))))))
+;; (inc (inc (inc (inc (inc 16)))))
+;; (inc (inc (inc (inc 17))))
+;; (inc (inc (inc 18)))
+;; (inc (inc 19))
+;; (inc 20)
+;; 21
 
 
-; Now we can apply the double procedure to this:
+; So we got the answer of 21, which agrees with what we get when we run
+; the expression normally:
 
-(double (double double))
-
-
-
-
-; Now we apply that whole thing to the inc function, and it should
-; wind up quite nicely.
-
-((double (double double)) inc)
-
-
-
-    
-     
-
+(((double (double double)) inc) 5)
+;> 21
